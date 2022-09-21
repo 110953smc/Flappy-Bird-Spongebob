d37d84152ee45420e8da0458a7f8c21170e873f1
Ranya Al <95483934+110953smc@users.noreply.github.com>
2022-09-21 11:45:34 +0000

    test

--- algemeen.js
+++ algemeen.js
@@ -1,26 +1,58 @@
-
-var bird;
-var pipes = [];
-
 function setup() {
-	createCanvas(400, 400,);
-  bird = new Bird();
-  pipes.push(new Pipe());
+	createCanvas(500, 400);
 }
 
 function draw() {
-  background(0);
-  bird.fall();
-  bird.showBird();
+  background(225);
 
-  for (var i = 0; i < pipes.length; i++) {
-    pipes[i].show();
-    pipes[i].update();
-  }
+ fill(0);
+ text("Gebruik de pijltjes op je keyboard om te bewegen, 25, 25");
+
+	fill(0, 255, 0);
+	rect(xpos, ypos, 50, 50);
+
+	if(xpos >= 0 && xpos + 50 <= 500) xpos += xspeed;
+	if(ypos >= 0 && ypos + 50 <= 500) ypos += yspeed;
 }
 
 function keyPressed() {
-  if (key == " ") {
-    bird.up();
-  }
+	switch(keyCode) {
+		case 37:
+		case 65:
+			xspeed = -2;
+			break;
+		case 39:
+		case 68:
+			xspeed = 2;
+			break;
+		case 38:
+		case 87:
+			yspeed = -2;
+			break;
+		case 40:
+		case 83:
+			yspeed = 2;
+			break;
+	}
+}
+
+function keyReleased() {
+	switch(keyCode) {
+		case 37:
+		case 65:
+			xspeed = 0;
+			break;
+		case 39:
+		case 68:
+			xspeed = 0;
+			break;
+		case 38:
+		case 87:
+			yspeed = 0;
+			break;
+		case 40:
+		case 83:
+			yspeed = 0;
+			break;
+	}
 }
