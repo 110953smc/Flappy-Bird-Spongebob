3d3ea6112cf5870075e32af024f149c067311346
Ranya Al <95483934+110953smc@users.noreply.github.com>
2022-09-21 11:48:48 +0000

    je kan nu alleen op en neer, canvas veranderd

--- algemeen.js
+++ algemeen.js
@@ -1,15 +1,13 @@
 function setup() {
-	createCanvas(500, 400);
+	createCanvas(300, 600);
 }
+
+var [xpos, ypos, xspeed, yspeed] = [150, 300, 0, 0];
 
 function draw() {
   background(225);
-
- fill(0);
- text("Gebruik de pijltjes op je keyboard om te bewegen, 25, 25");
-
-	fill(0, 255, 0);
-	rect(xpos, ypos, 50, 50);
+ fill(0, 150, 0);
+	ellipse(xpos, ypos, 50, 50);
 
 	if(xpos >= 0 && xpos + 50 <= 500) xpos += xspeed;
 	if(ypos >= 0 && ypos + 50 <= 500) ypos += yspeed;
@@ -17,42 +15,16 @@ function draw() {
 
 function keyPressed() {
 	switch(keyCode) {
-		case 37:
-		case 65:
-			xspeed = -2;
-			break;
-		case 39:
-		case 68:
-			xspeed = 2;
-			break;
-		case 38:
-		case 87:
-			yspeed = -2;
-			break;
-		case 40:
-		case 83:
-			yspeed = 2;
-			break;
+	case 38:
+	case 87:
+	speed = -2;
 	}
 }
 
 function keyReleased() {
 	switch(keyCode) {
-		case 37:
-		case 65:
-			xspeed = 0;
-			break;
-		case 39:
-		case 68:
-			xspeed = 0;
-			break;
-		case 38:
-		case 87:
-			yspeed = 0;
-			break;
-		case 40:
-		case 83:
-			yspeed = 0;
-			break;
+	case 38:
+	case 87:
+	yspeed = 0;
 	}
 }
